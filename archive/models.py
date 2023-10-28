1941e11967dba617314dcff12dfde7c370e64ac2
Gzh0821 <gaozih0821@outlook.com>
2023-10-28 23:30:15 +0800

    add chat archive.

---
+++ archive/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class ChatArchive(models.Model):
+    # 用户
+    user = models.ForeignKey(
+        User,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='ArchiveToUser'
+    )
+    # 聊天内容
+    body = models.JSONField()
+    # 时间戳
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return
