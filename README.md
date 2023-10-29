afe1f42ad5a6b86fccc50525dd66ff609b26f2cb
Gzh0821 <gaozih0821@outlook.com>
2023-10-29 17:59:53 +0800

    rewrite README.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# glmServer
-A Server for ChatGLM.
+# GLMServer
+
+A Simple Django Server for ChatGLM.
+
+![](https://img.shields.io/badge/author-Gaozih-%2366ccff)
+![](https://img.shields.io/github/license/Gzh0821/Optimization_project)
+
+
+## Quickstart
+- 安装python 3.11
+- 安装本项目依赖:
+```
+    pip install -r requirements.txt
+```
+- 建立项目所需的数据库(SQLite):
+```
+    python manage.py makemigrations
+    python manage.py migrate
+```
+- 创建超级管理员,按提示输入用户名和密码:
+```
+    python manage.py createsuperuser
+```
+- 启动开发用服务器,设置端口号为8000(可更改):
+```
+    python manage.py runserver 8000
+```
+- 访问`http://127.0.0.1:8000`,进入swagger api文档界面.
+## Path
+- `/api`:api接口
+- `/admin`:django管理员界面
+- `/swagger`:Swagger-ui风格的api文档
+- `/redoc`:Redoc风格的api文档
+- `/temp/auth/`:DRF提供的临时用户登录/注销界面
+
--- glmServer/urls.py
+++ glmServer/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -18,10 +19,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='swagger/', permanent=False)),
     path("admin/", admin.site.urls),
     path('api/archive/', include('archive.urls', namespace='archive')),
     path('api/user/', include('userprofile.urls', namespace='userprofile')),
-    path('api/temp/auth/', include('rest_framework.urls')),
+    path('temp/auth/', include('rest_framework.urls')),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
