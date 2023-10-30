4f7677ff61864167073f2ab843197b6c0a6cd7c8
Gzh0821 <gaozih0821@outlook.com>
2023-10-30 23:55:27 +0800

    add Qdana workflow.

--- archive/invoke.py
+++ archive/invoke.py
@@ -3,13 +3,23 @@ from asgiref.sync import async_to_sync
 from django.conf import settings
 from transformers import AutoModel, AutoTokenizer
 
-TEMPLATE = '''
+TEMPLATE = """
 以下提示用于指导Al绘画模型创建图像。它们包括人物外观、背景、颜色和光影效果，以及图像的主题和风格等各种细节。这些提示的格式通常包括带权重的数字括号，用于指定某些细节的重要性或强调。例如，"(masterpiece:1.4)"表示作品的质量非常重要。以下是三个示例：
-1. (8k, RAW photo, best quality, masterpiece:1.2),(realistic, photo-realistic:1.37), ultra-detailed, 1girl, cute, solo, beautiful detailed sky, detailed cafe, night, sitting, dating, (nose blush), (smile:1.1),(closed mouth), medium breasts, beautiful detailed eyes, (collared shirt:1.1), bowtie, pleated skirt, (short hair:1.2), floating hair, ((masterpiece)), ((best quality)),
-2. (masterpiece, finely detailed beautiful eyes: 1.2), ultra-detailed, illustration, 1 girl, blue hair black hair, japanese clothes, cherry blossoms, tori, street full of cherry blossoms, detailed background, realistic, volumetric light, sunbeam, light rays, sky, cloud,
-3. highres, highest quallity, illustration, cinematic light, ultra detailed, detailed face, (detailed eyes, best quality, hyper detailed, masterpiece, (detailed face), blue hairlwhite hair, purple eyes, highest details, luminous eyes, medium breats, black halo, white clothes, backlighting, (midriff:1.4), light rays, (high contrast), (colorful)
 
-仿照之前的提示，使用英文，写一段描写如下要素的提示：'''
+第一个: (1 girl, 8k, RAW photo, best quality, masterpiece:1.2),(realistic, photo-realistic:1.37), ultra-detailed, 1girl, 
+cute, solo, beautiful detailed sky, detailed cafe, night, sitting, dating, (nose blush), (smile:1.1),(closed mouth), 
+medium breasts, beautiful detailed eyes, (collared shirt:1.1), bowtie, pleated skirt, (short hair:1.2), 
+floating hair, ((masterpiece)), ((best quality)),
+
+第二个: (masterpiece, finely detailed beautiful eyes: 1.2), ultra-detailed, illustration, 1 girl, blue hair black hair, 
+japanese clothes, cherry blossoms, tori, street full of cherry blossoms, detailed background, realistic, volumetric 
+light, sunbeam, light rays, sky, cloud,
+
+第三个: highest quallity, illustration, cinematic light, ultra detailed, detailed face, (detailed eyes, best quality, 
+hyper detailed, masterpiece, (detailed face), blue hairlwhite hair, purple eyes, highest details, luminous eyes, 
+medium breats, black halo, white clothes, backlighting, (midriff:1.4), light rays, (high contrast), (colorful)
+
+上面是三个提示的示例，仿照这些示例，使用英文，写一段描写如下要素的提示："""
 
 PAYLOAD_TEMPLATE = {
     "negative_prompt": "nsfw,logo,text,badhandv4,EasyNegative,ng_deepnegative_v1_75t,rev2-badprompt,"
