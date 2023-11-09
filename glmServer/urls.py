699c523d76ab1e5fc6e4bfa580abd88c9547a37a
Gzh0821 <gaozih0821@outlook.com>
2023-11-10 00:56:40 +0800

    Add release options.

--- glmServer/urls.py
+++ glmServer/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+
 from glmServer.views import teapot
 
 schema_view = get_schema_view(
@@ -20,14 +22,18 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='swagger/', permanent=False)),
-    path("admin/", admin.site.urls),
     path('api/teapot/', teapot, name='teapot'),
     path('api/archive/', include('archive.urls', namespace='archive')),
     path('api/user/', include('userprofile.urls', namespace='userprofile')),
     path('api/admin/', include('adminfunc.urls', namespace='adminfunc')),
-    path('temp/auth/', include('rest_framework.urls')),
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('', RedirectView.as_view(url='swagger/', permanent=False)),
+        path("admin/", admin.site.urls),
+        path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+        path('temp/auth/', include('rest_framework.urls')),
+    ]
