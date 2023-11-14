1741e6ef828bba92906481b3ef867ea425070763
Gzh0821 <gaozih0821@outlook.com>
2023-11-14 21:52:05 +0800

    optimization model.

--- archive/invoke.py
+++ archive/invoke.py
@@ -4,16 +4,16 @@ import requests
 from asgiref.sync import async_to_sync
 from django.conf import settings
 from transformers import AutoModel, AutoTokenizer
-from diffusers import DiffusionPipeline
+from diffusers import StableDiffusionXLPipeline
 import torch
 from io import BytesIO
 
 TEMPLATE = """
-以下提示用于指导Al绘画模型创建图像。它们包括外观和背景描述、颜色、图片质量，以及图像的主题和风格等各种细节。以下是三个示例:
+以下简短的提示用于指导Al绘画模型创建图像。它们包括外观和背景描述、颜色、图片质量，以及图像的主题和风格等各种细节。下面是三个示例:
 第一个:"Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
 第二个:"A majestic lion jumping from a big stone at night"
 第三个:"sailing ship in storm by Leonardo da Vinci"
-上面是三个提示的示例，仿照这些示例，使用英文，写一段描写如下要素的提示："""
+上面是三个提示的示例，仿照这些示例，使用英文，简略的写一段描写如下要素的提示，不要超过60词："""
 
 PAYLOAD_TEMPLATE = {
     "negative_prompt": "nsfw,logo,text,badhandv4,EasyNegative,ng_deepnegative_v1_75t,rev2-badprompt,"
@@ -28,18 +28,20 @@ PAYLOAD_TEMPLATE = {
     "tiling": "false",
     "denoising_strength": 0.75,
 }
+torch.backends.cuda.matmul.allow_tf32 = True
 
 tokenizer = AutoTokenizer.from_pretrained(settings.GLM_MODEL_PATH, trust_remote_code=True)
 model = AutoModel.from_pretrained(settings.GLM_MODEL_PATH, trust_remote_code=True).cuda()
 model = model.eval()
 
-baseSDXL = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0",
+baseSDXL = StableDiffusionXLPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0",
                                          torch_dtype=torch.float16,
                                          use_safetensors=True,
                                          variant="fp16",
-                                         add_watermarker=False,
-                                         trust_remote_code=True).to("cuda")
+                                         add_watermarker=False)
+baseSDXL.enable_model_cpu_offload()
 
+baseSDXL.enable_vae_slicing()
 
 @async_to_sync
 async def generate_text(input_text: str) -> str:
